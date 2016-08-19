33600993987c8bba0fdd3dc8e35906dc3b682471
luddeo <privat@eriklundin.nu>
2016-08-19 14:53:32 +0200

    Use bigmemory for intensity matrix, as data takes up to much memory.

--- 1-make_images.R
+++ 1-make_images.R
@@ -4,56 +4,7 @@
 # sample.                                                                      #
 ################################################################################
 
-require(png)
-require(gtools)
 source("library.R")
-
-make_position_matrix <- function(l_project, l_sample, l_height) {
-  # Makes a matrix of the scan number on a line (it is the same as the old scan
-  # matrix). Need to add the line too, so that it can be used to make an image
-  # with the correct measure of the m/z at the correct position.
-  header_files <- paste(l_project[[l_sample]][["header folder"]],
-                        sort(list.files(l_project[[l_sample]][["header folder"]])),
-                        sep="/")
-  
-  # Get the max number of scans in a line.
-  max_line_length <- get_header_data(header_files)[["max line"]]
-  position_matrix <- c()
-  
-  for(t_line in seq(header_files)) {
-    print(paste("Line:", t_line))
-    
-    header_matrix <- read_header_file(header_files[t_line])
-    # Calculate the number of pixels needed to add to the line to make it
-    # the same length as the longest line
-    extra_lines <- max_line_length - nrow(header_matrix)
-    # Get list of the time length of each scan
-    time_list <- diff(c(0,header_matrix[,"RT"]))
-    # The cutoff the the pixels with time above it will be extended on pixel
-    time_cutoff <- ifelse(extra_lines == 0, Inf,
-                          min(rev(sort(time_list))[extra_lines]))
-    line_vector <- c()
-    for(t_scan in header_matrix[,"SN"]) {
-      IT <- header_matrix[header_matrix[,"SN"] == t_scan,"IT"]
-      scan_rep <- 1
-      if((time_list[t_scan] >= time_cutoff) & (extra_lines > 0)) {
-        # If there are to many pixels with high IT there will be
-        # more pixels with a IT above the cutoff, so only extend the first ones.
-        scan_rep <- 2
-        extra_lines <- extra_lines - 1
-      }
-      line_vector <- c(line_vector, rep(paste(t_line, t_scan, sep=":"), 
-                                        scan_rep))
-    }
-    image_line <- c()
-    for(i in 1:l_height) {
-      # Make the line wider
-      image_line <- rbind(image_line, line_vector)
-    }
-    position_matrix <- rbind(position_matrix, image_line)
-  }
-  return(position_matrix)
-}
 
 make_intensity_image_matrix <- function(l_position_matrix,l_intensity_values) {
   outlier_limit <- boxplot.stats(as.numeric(l_intensity_values))$stats[5]
--- library.R
+++ library.R
@@ -3,6 +3,10 @@
 # Contains functions that are used in (or expected to) more than one script.   #
 ################################################################################
 
+require(png)
+require(gtools)
+require(bigmemory)
+
 read_peak_file <- function(l_file) {
   return(read.csv(l_file, sep="\t"))
 }
@@ -16,10 +20,8 @@ read_roi_csv_file <- function(l_file) {
 }
 
 read_intensity_matrix <- function(l_name) {
-  return(read.table(l_name, sep="\t",
-                    stringsAsFactors = FALSE,
-                    header = TRUE, row.names = 1,
-                    check.names = FALSE))
+  return(read.big.matrix(l_name, sep="\t", has.row.names = T,
+                         header=TRUE, type="double"))
 }
 
 get_tolerance <- function(l_peak_matrix) {
@@ -68,4 +70,52 @@ anti_analising_fix <- function(l_roi_image) {
   t_r <- c(which.max(t_roi_table))
   l_roi_image[l_roi_image[,,3] < as.numeric(names(t_roi_table[t_r - 1]))] <- 0
   return(l_roi_image)
+}
+
+
+make_position_matrix <- function(l_project, l_sample, l_height) {
+  # Makes a matrix of the scan number on a line (it is the same as the old scan
+  # matrix). Need to add the line too, so that it can be used to make an image
+  # with the correct measure of the m/z at the correct position.
+  header_files <- paste(l_project[[l_sample]][["header folder"]],
+                        sort(list.files(l_project[[l_sample]][["header folder"]])),
+                        sep="/")
+  
+  # Get the max number of scans in a line.
+  max_line_length <- get_header_data(header_files)[["max line"]]
+  position_matrix <- c()
+  
+  for(t_line in seq(header_files)) {
+    print(paste("Line:", t_line))
+    
+    header_matrix <- read_header_file(header_files[t_line])
+    # Calculate the number of pixels needed to add to the line to make it
+    # the same length as the longest line
+    extra_lines <- max_line_length - nrow(header_matrix)
+    # Get list of the time length of each scan
+    time_list <- diff(c(0,header_matrix[,"RT"]))
+    # The cutoff the the pixels with time above it will be extended on pixel
+    time_cutoff <- ifelse(extra_lines == 0, Inf,
+                          min(rev(sort(time_list))[extra_lines]))
+    line_vector <- c()
+    for(t_scan in header_matrix[,"SN"]) {
+      IT <- header_matrix[header_matrix[,"SN"] == t_scan,"IT"]
+      scan_rep <- 1
+      if((time_list[t_scan] >= time_cutoff) & (extra_lines > 0)) {
+        # If there are to many pixels with high IT there will be
+        # more pixels with a IT above the cutoff, so only extend the first ones.
+        scan_rep <- 2
+        extra_lines <- extra_lines - 1
+      }
+      line_vector <- c(line_vector, rep(paste(t_line, t_scan, sep=":"), 
+                                        scan_rep))
+    }
+    image_line <- c()
+    for(i in 1:l_height) {
+      # Make the line wider
+      image_line <- rbind(image_line, line_vector)
+    }
+    position_matrix <- rbind(position_matrix, image_line)
+  }
+  return(position_matrix)
 }
